3d8096cf35a04d423efd71d1fdd7615c4d49ad25
Ildar Babataev <ildar.babataev@evocargo.com>
2025-08-28 20:35:32 +0000

    reorganized and add cmake.in files

---
+++ core/include/MANTruckDataset/MANTruckDataset.hpp
@@ -0,0 +1,9 @@
+#include <string>
+
+class MANTruckDataset {
+public:
+  explicit MANTruckDataset(const std::string& dataset_folder);
+
+private:
+
+};
---
+++ core/src/MANTruckDataset/MANTruckDataset.cpp
@@ -0,0 +1,6 @@
+#include "MANTruckDataset/MANTruckDataset.hpp"
+
+MANTruckDataset::MANTruckDataset(const std::string& dataset_folder)
+{
+  
+}
