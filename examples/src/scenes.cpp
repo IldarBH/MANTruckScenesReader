931c5dc6e28920f03785c535339113e540649822
Ildar Babataev <ildar.babataev@evocargo.com>
2025-11-20 23:54:58 +0000

    added simple scene reading example

---
+++ examples/src/scenes.cpp
@@ -0,0 +1,24 @@
+#include <MANTruckDataset/Scene.hpp>
+
+#include <string>
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+namespace man_ds = man::dataset;
+
+int main(int argc, char** argv){
+  if (argc < 2){
+    std::cout << "Usage: './example <dataset_path>'";
+    return -1;
+  }
+  const fs::path dataset_path(argv[1]);
+  const std::string samples_file(dataset_path / "v1.0-mini" / "scene.json");
+  
+  man_ds::scenes::SceneManager scene_manager;
+  scene_manager.read_scenes(samples_file);
+  for (const auto& scene : scene_manager){
+    std::cout << scene << std::endl;
+  }
+  return 0;
+}
