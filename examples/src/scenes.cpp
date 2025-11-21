73bc947669503ec5264f3eb71c3f2a6346d4767c
Ildar Babataev <ildar.babataev@evocargo.com>
2025-11-21 00:03:52 +0000

    added exmaple scene selection

--- examples/src/scenes.cpp
+++ examples/src/scenes.cpp
@@ -17,8 +17,22 @@ int main(int argc, char** argv){
   
   man_ds::scenes::SceneManager scene_manager;
   scene_manager.read_scenes(samples_file);
-  for (const auto& scene : scene_manager){
-    std::cout << scene << std::endl;
+  std::cout << "Select scene to load:" << std::endl;
+  for (size_t id = 0; id < scene_manager.size(); ++id){
+    std::cout << id+1 << ". " << scene_manager[id].NAME << std::endl;
   }
+  std::cout << "Enter selection (1-" << scene_manager.size() << "): ";
+
+  size_t selection = 0;
+  while (selection < 1 || selection > scene_manager.size()){
+    std::cin >> selection;
+    if (selection < 1 || selection > scene_manager.size()){
+      std::cerr << "Invalid selection! Try again." << std::endl;
+    }
+  }
+  
+  const auto& scene = scene_manager[selection - 1];
+  std::cout << "You have selected scene:" << std::endl;
+  std::cout << scene << std::endl;
   return 0;
 }
