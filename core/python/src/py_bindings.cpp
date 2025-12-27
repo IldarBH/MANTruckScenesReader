5c5acfba46399cef43a648c15077756d50b209a6
Ildar Babataev <ildar.babataev@evocargo.com>
2025-12-27 22:12:34 +0000

    update sensor pybindings

--- core/python/src/py_bindings.cpp
+++ core/python/src/py_bindings.cpp
@@ -5,7 +5,7 @@ namespace py = pybind11;
 // Forward declarations
 void init_utils_bindings(py::module &m);
 void init_scene_bindings(py::module &m);
-// void init_sample_bindings(py::module &m);
+void init_sample_bindings(py::module &m);
 // void init_sensor_bindings(py::module &m);
 // void init_dataset_bindings(py::module &m);
 
@@ -14,7 +14,7 @@ PYBIND11_MODULE(MANTruckDatasetPy, m) {
   
   init_utils_bindings(m);
   init_scene_bindings(m);
-  // init_sample_bindings(m);
+  init_sample_bindings(m);
   // init_sensor_bindings(m);
   // init_dataset_bindings(m);
 }
--- examples/py/src/samples.py
+++ examples/py/src/samples.py
@@ -1,8 +1,8 @@
-from MANTruckDatasetPy import Sample, SampleSequence
+from MANTruckDatasetPy import Sample, SampleSequence, Token
 from utils import SAMPLE_FILE
 
 if __name__ == "__main__":
   sample_mgr = SampleSequence()
-  sample_mgr.read_samples(str(SAMPLE_FILE), "073e6bb18188424ea9981e3845df54a6")
+  sample_mgr.read_samples(str(SAMPLE_FILE), Token("073e6bb18188424ea9981e3845df54a6"))
   for sample in sample_mgr:
-    print(f"Sample token: {sample.get_token()}, Timestamp: {sample.get_timestamp()}")
+    print(f"Sample token: {sample.get_token().value}, Timestamp: {sample.get_timestamp()}")
